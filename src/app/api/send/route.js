ea6e92e091d2ba2fa319e2d422d2108a8fb44ed6
Kuladip Das <165942926+kuladip12@users.noreply.github.com>
2024-08-31 13:53:21 +0530

    new commit

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -4,26 +4,25 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req) {
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
-    const { email, subject, message } = await req.json();
-    console.log(email, subject, message);
-
     const data = await resend.emails.send({
       from: fromEmail,
       to: [fromEmail, email],
       subject: subject,
-      html: `
-        <h1>${subject}</h1>
-        <p>Thank you for contacting us!</p>
-        <p>New message submitted:</p>
-        <p>${message}</p>
-      `,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
     });
-
     return NextResponse.json(data);
   } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: error.message });
+    return NextResponse.json({ error });
   }
 }
